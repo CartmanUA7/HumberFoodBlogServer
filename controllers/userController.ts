1df774db8da636c6feeba08e5b21e781d71ff514
Vasyl Marusiak <cartmanUA7@gmail.com>
2023-12-06 21:43:27 -0500

    update bcrypt

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { User } from "../models/Users";
 
